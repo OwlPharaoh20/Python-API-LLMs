670be7b4a9b1214e7d332fd1559fd850ef451c35
GG-Pharaoh <oxdiablo20@gmail.com>
2025-05-10 05:13:07 -0700

    Test file added

--- main.py
+++ main.py
@@ -12,7 +12,18 @@ print(API_KEY_CREDITS)
 app = FastAPI()
 
 
+def verify_api_key(x_api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(x_api_key, 0)
+    if credits <= 0:
+        raise HTTPException(status_code=401, detail="Invalid API Key, or no credits")
+
+    return x_api_key
+
+
+
 @app.post("/generate")
-def generate(prompt:str):
-    response = ollama.chat(model="mistral", messages=[{"role": "user", "content" : prompt }])
-    return {"response" : response["message"] ["content"]}
+def generate(prompt: str, x_api_key: str = Depends(verify_api_key)):
+    API_KEY_CREDITS[x_api_key] -= 1
+    response = ollama.chat(model="mistral", messages=[{"role": "user", "content": prompt}])
+    return {"response": response["message"]["content"]}
+
